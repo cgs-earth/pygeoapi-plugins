b1aea24fbfd5bf9f5165bb277bfc55187fd569f1
webb-ben <bwebb@lincolninst.edu>
2024-03-25 19:00:05 -0400

    Update test_sitemap_process.py

--- tests/test_sitemap_process.py
+++ tests/test_sitemap_process.py
@@ -60,7 +60,7 @@ def test_sitemap_generator(body):
     assert len(sitemap) == 5
 
     common = sitemap.pop('common.xml')
-    assert len(common) == 2279
+    assert len(common) == 2402
 
     root = xml.etree.ElementTree.fromstring(common)
     assert all(i.tag == j.tag for (i, j) in zip(root, root.findall('url')))
@@ -85,7 +85,7 @@ def test_sitemap_no_features(body):
     assert len(sitemap) == 1
 
     common = sitemap.pop('common.xml')
-    assert len(common) == 2279
+    assert len(common) == 2402
 
 
 def test_sitemap_zip(body):
