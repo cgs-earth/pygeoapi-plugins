d0f31d1ee8125a2c21c645416f10d575ebdc4469
Benjamin Webb <benjamin.miller.webb@gmail.com>
2025-05-13 16:55:06 -0400

    Update copyright year

--- tests/test_xml_formatter.py
+++ tests/test_xml_formatter.py
@@ -2,7 +2,7 @@
 #
 # Author: Benjamin Webb <bwebb@lincolninst.edu>
 #
-# Copyright (c) 2023 Center for Geospatial Solutions
+# Copyright (c) 2025 Center for Geospatial Solutions
 #
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
